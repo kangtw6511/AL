f95b2ed0087d9aa110386dd99e7e7a3dc80e1e84
kangtw6511 <115384348+kangtw6511@users.noreply.github.com>
2023-04-20 05:34:36 +0900

    Add files via upload

--- optimize.sh
+++ optimize.sh
@@ -31,8 +31,30 @@ net.ipv4.tcp_fin_timeout = 20
 # Increase the maximum size of the receive and send buffers
 net.core.rmem_max = 56623104
 net.core.wmem_max = 56623104
+
+# Optimize disk I/O performance
+vm.swappiness = 10
+vm.vfs_cache_pressure = 50
+
+# Increase the maximum number of open files and processes
+fs.file-max = 2097152
+kernel.pid_max = 4194304
+
+# Increase the maximum number of memory map areas per process
+vm.max_map_count = 262144
+
+# Enable transparent huge pages
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never > /sys/kernel/mm/transparent_hugepage/defrag
+
+# Reduce kernel message log level
+kernel.printk = 3 3 3 3
+
+# Disable kernel modules not needed for your hardware
+sudo tee /etc/modprobe.d/blacklist.conf >/dev/null <<EOF
+blacklist floppy
+blacklist pcspkr
 EOF
-sudo sysctl -p /etc/sysctl.d/99-sysctl.conf
 
 # Set GPU performance profile to high
 gpu_freq="$(sudo intel_gpu_top -o - | head -n 1 | awk '{print $1}')"
@@ -44,3 +66,4 @@ else
 fi
 
 echo "System optimization complete."
+
